b37c943c40cf87bd448c3a6eda0b608405447c76
Artur Alves Pereira Machado <aalves-p@c2r6s3.42lisboa.com>
2022-12-06 00:51:27 +0000

    Done but malloc

--- ft_getnextline/get_next_line.c
+++ ft_getnextline/get_next_line.c
@@ -6,14 +6,16 @@
 /*   By: aalves-p <aalves-p@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 01:35:12 by aalves-p          #+#    #+#             */
-/*   Updated: 2022/12/05 22:06:23 by aalves-p         ###   ########.fr       */
+/*   Updated: 2022/12/06 00:48:20 by aalves-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //Create static var for buffersize + 1 for NULL
 //Create string which will have all the Lines
 //Make Condition for -1 on read and if BUFFER is smaller than 1
-//Initialize string as NULL
+//Initialize string reate static var for buffersize + 1 for NULL
+//Create string which will have all the Lines
+//Make Condition for -1 onas NULL
 //Send string to join if my stash is not 0 first position and my read is > 0
 #include "get_next_line.h"
 
@@ -21,28 +23,36 @@ char	*get_next_line(int fd)
 {
 	static char	stash[BUFFER_SIZE + 1];
 	char		*str;
+	int			i;
 
+	i = -1;
 	if (read(fd, 0, 0) < 0 || BUFFER_SIZE < 1)
+	{
+		while (stash[++i])
+			stash[i] = 0;
 		return (0);
+	}
 	str = NULL;
-	while (read(fd, stash, BUFFER_SIZE) > 0 && stash[0] != 0)
+	while (stash[0] || read(fd, stash, BUFFER_SIZE) > 0)
 	{
 		str = ft_strjoin(stash, str);
 		if (ft_check(stash) >= 0)
 		{
-			printf("Stash %s\n", stash);
 			break ;
 		}
 	}
 	return (str);
 }
 
-int main()
+/* int main()
 {
 	int fd = open("file.txt", O_RDONLY);
 	printf("fd Value %d\n", fd);
 	char *str1 = get_next_line(fd);
 	char *str2 = get_next_line(fd);
-	printf("Read this %s", str1);
-	printf("Read this %s", str2);
+	char *str3 = get_next_line(fd);
+	printf("Read this %s\n", str1);
+	printf("Read this %s\n", str2);
+	printf("Read this %s\n", str3);
 }
+ */
--- ft_getnextline/get_next_line_utils.c
+++ ft_getnextline/get_next_line_utils.c
@@ -6,13 +6,13 @@
 /*   By: aalves-p <aalves-p@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 18:34:51 by aalves-p          #+#    #+#             */
-/*   Updated: 2022/12/05 22:09:45 by aalves-p         ###   ########.fr       */
+/*   Updated: 2022/12/06 00:49:40 by aalves-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_strlen(char *str)
+int	ft_strlen(const char *str)
 {
 	int	i;
 
@@ -21,37 +21,34 @@ int	ft_strlen(char *str)
 		return (0);
 	while (str[i] && str[i] != '\n')
 		i++;
-	return (i  + (str[i] == '\n'));
+	return ((i + (str[i] == '\n')));
 }
 
 char	*ft_strjoin(char *stash, char *str)
 {
 	int			i;
-	int			j;
 	char		*line;
+	int			j;
 
-	i = 0;
 	j = 0;
-
-	line = (char *)malloc(ft_strlen(stash) + ft_strlen(str) + 1);
+	i = 0;
+	//printf("String size %d, Stash Size %d", ft_strlen(str), ft_strlen(stash));
+	line = (char *)malloc(ft_strlen(str) + ft_strlen(stash) + 1 * sizeof(char));
 	if (!line)
 		return (0);
-	if (str)
+	while (str && str[i])
 	{
-		while (str[i] && str)
-		{
-			line[i] = str[i];
-			i++;
-		}
-		free (str);
+		line[i] = str[i];
+		i++;
 	}
 	while (stash[j] && stash)
 	{
+		line[i++] = stash[j++];
 		if (stash[j] == '\n')
 			break ;
-		line[i++] = stash[j++];
 	}
 	line[i] = 0;
+	free (str);
 	return (line);
 }
 
@@ -60,23 +57,24 @@ int	ft_check(char *stash)
 	int	i;
 	int	j;
 
-	i = 0;
+	i = -1;
 	j = -1;
-	while (stash[i])
+	while (stash[++i])
 	{
 		if (stash[i] == '\n')
 		{
+			stash[i++] = 0;
 			j = 0;
 			break ;
 		}
 		stash[i] = 0;
-		i++;
 	}
 	if (j == 0)
 	{
-		while (stash[j] != '\n' && stash[j] && stash[i])
+		while (stash[i])
 		{
-			stash[j++] = stash[i++];
+			stash[j++] = stash[i];
+			stash[i++] = 0;
 		}
 	}
 	return (j);
